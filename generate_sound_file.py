0caa35ec77c1d891081c67a8493cc4287b133f7c
Salma-H <Salma.h.bahy@gmail.com>
2021-04-11 14:42:06 +0200

    ui updated

--- generate_sound_file.py
+++ generate_sound_file.py
@@ -12,14 +12,14 @@ sample_rate = 44100.0
 
 
 
-def create_signal(f1 = 500, f2 = 600, f3 = 880, f4 = 0 , f5 = 0, duration_seconds = 2):
+def create_signal(f1 = 500, f2 = 600, f3 = 880, f4 = 700 , f5 = 1050, duration_seconds = 2):
     global audio
     num_samples = duration_seconds * sample_rate
 
     for x in range(int(num_samples)):
         sig1 = math.sin(2 * math.pi * f1 * ( x / sample_rate ))
-        sig2 = math.cos(2 * math.pi * f2 * ( x / sample_rate ))
-        sig3 = math.cos(2 * math.pi * f3 * ( x / sample_rate ))
+        sig2 = math.sin(2 * math.pi * f2 * ( x / sample_rate ))
+        sig3 = math.sin(2 * math.pi * f3 * ( x / sample_rate ))
         sig4 = math.sin(2 * math.pi * f4 * ( x / sample_rate ))
         sig5 = math.sin(2 * math.pi * f5 * ( x / sample_rate ))
         audio.append(sig1 + sig2 + sig3 + sig4 + sig5)
